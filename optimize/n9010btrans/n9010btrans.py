cc34e81cc5893280392eb7a5c27036435c69354f
Ruofan He <67919467+ruofan-he@users.noreply.github.com>
2022-09-06 14:45:45 +0900

    Add files via upload

---
+++ optimize/n9010btrans/n9010btrans.py
@@ -0,0 +1,21 @@
+import string
+import pyvisa
+import numpy as np
+
+# データだけ転送する
+class N9010BtransData:
+    def __init__(self, visa_address):
+        self.visa_address = visa_address
+        self.rm = pyvisa.ResourceManager()
+        with self.rm.open_resource(self.visa_address) as inst:
+            print(inst.query('*IDN?'))
+
+    def transfer(self, channel):
+        assert channel in [1,2,3,4,5,6]
+        trace = None
+        with self.rm.open_resource(self.visa_address) as inst:
+            result = inst.query("TRAC? TRACE{channel}")
+            trace = np.array(list(map(float,result.split(','))))
+        return trace
+
+    
